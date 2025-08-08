9b4c9dab9b1cbd587d6f185670a71aeba65d5da4
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2025-08-08 15:41:11 +0600

    modify

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
-
 app.use(globalErrorHandler);
 
 app.get("/", (req: Request, res: Response) => {
